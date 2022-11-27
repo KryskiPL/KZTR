1f4c9dbf78350259755c17179df196d9b035499f
KryskiPL <93291497+KryskiPL@users.noreply.github.com>
2022-11-27 23:07:28 +0100

    C1 program
    a or b haiest

---
+++ C1/test1/test1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    int a, b;
+
+    cout << "Hello World!" << " ";
+    cin >> a >> b;
+        if (a >= b) {
+            cout << "=: " << a;
+        }
+        else {
+            cout << "=: " << b;
+        }
+
+    return 0;
+}
